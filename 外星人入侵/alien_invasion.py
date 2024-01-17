2e578322470bdfa3debd33ef2b161050403edc15
Mathew-Carl <150131584+Mathew-Carl@users.noreply.github.com>
2024-01-17 21:02:33 +0800

    Co-authored-by: Mathew-Carl <150131584+Mathew-Carl@users.noreply.github.com> Co-authored-by: lichuan <1942442593@qq.com>

--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -24,19 +24,19 @@ class AlienInvasion:
             
     def _check_events(self):
             #监听鼠标与键盘事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                          self.ship.moving_right = True
-                    if event.key == pygame.K_LEFT:
-                          self.ship.moving_left = True
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                          self.ship.moving_right = False
-                    if event.key == pygame.K_LEFT:
-                          self.ship.moving_left = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
                 
 
     def _update_screen(self):
