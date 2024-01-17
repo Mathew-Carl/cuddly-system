bbb3213e21ece19fcc1869376c85b6ff2e04efd7
Mathew-Carl <150131584+Mathew-Carl@users.noreply.github.com>
2024-01-17 20:02:44 +0800

    Co-authored-by: Mathew-Carl <150131584+Mathew-Carl@users.noreply.github.com> Co-authored-by: lichuan <1942442593@qq.com>
    undefined
    
    undefined

--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -17,17 +17,38 @@ class AlienInvasion:
     def run_game(self):
         #开始游戏的主循环
         while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(60)#帧率控制
+            
+    def _check_events(self):
             #监听鼠标与键盘事件
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                          self.ship.moving_right = True
+                    if event.key == pygame.K_LEFT:
+                          self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                          self.ship.moving_right = False
+                    if event.key == pygame.K_LEFT:
+                          self.ship.moving_left = False
+                
+
+    def _update_screen(self):
             #每次循环重绘屏幕
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
             #最近绘制的屏幕可见
             pygame.display.flip()
-            self.clock.tick(60)#帧率控制
-            
+    
+    def _check_events(self):
+         """响应按键和鼠标事件"""
+
 if __name__ == "__main__":
     #创建实例并运行
     ai = AlienInvasion()
--- 外星人入侵/ship.py
+++ 外星人入侵/ship.py
@@ -6,6 +6,7 @@ class Ship:
         #初始化飞船及其位置
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect() 
+        self.settings = ai_game.settings
 
         #加载飞船图像并获取外接矩形
         self.image = pygame.image.load('images\ship.bmp')
@@ -13,6 +14,22 @@ class Ship:
 
         #飞船放入屏幕底部中央
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #储存速度浮点数
+        self.x = float(self.rect.x)
+
+        #移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """更新飞船位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
 
     def blitme(self):
         #指定位置放置飞船
