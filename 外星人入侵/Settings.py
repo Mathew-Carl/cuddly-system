4c529e6d300a89ed798b340fb53b120868bba784
Mathew-Carl <150131584+Mathew-Carl@users.noreply.github.com>
2024-01-20 22:07:11 +0800

    Co-authored-by: Mathew-Carl <150131584+Mathew-Carl@users.noreply.github.com> Co-authored-by: lichuan <1942442593@qq.com>

--- 外星人入侵/Settings.py
+++ 外星人入侵/Settings.py
@@ -13,6 +13,13 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 99999999
-        
-        
+        self.bullets_allowed = 3
+        '''无限子弹模式? 割草无双 超大子弹多种武器?'''
+
+        '''极速模式? 更改子弹以及飞船速度, 或者开放自定义端口?'''
+        #外星人设置
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        #fleet_direction 1为右面 -1为左面
+        self.fleet_direction = 1
+
--- 外星人入侵/alien_invasion.py
+++ 外星人入侵/alien_invasion.py
@@ -27,6 +27,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)#帧率控制
             self._update_bullets()
@@ -56,21 +57,31 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False       
-  
-    def _fire_bullet(self):
-        #创建子弹,并加入编组
-        if len(self.bullets) < self.settings.bullets_allowed:#子弹数设置
-            new_bullet = Bullet(self)
-            self.bullets.add(new_bullet)
+    
+    def _check_fleet_edges(self):
+        #外星人边缘策略
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        #向下移动并改变方向
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
 
     def _update_screen(self):
         #每次循环重绘屏幕
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-            #最近绘制的屏幕可见
+
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
-        
+
+        self.aliens.draw(self.screen)
+
         pygame.display.flip()
     
     def _update_bullets(self):
@@ -78,10 +89,40 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+
+    def _update_aliens(self):
+        #检查是否边缘并更新
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _create_fleet(self):
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size      
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x <= (self.settings.screen_width - 2 * alien_width):#余下的空间不少于外星人的两倍
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+
+            #添加一行后
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+            new_alien =Alien(self)
+            new_alien.x = x_position
+            new_alien.rect.x = x_position
+            new_alien.rect.y = y_position
+            self.aliens.add(new_alien)
+
+    def _fire_bullet(self):
+        #创建子弹,并加入编组
+        if len(self.bullets) < self.settings.bullets_allowed:#子弹数设置
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
 
 if __name__ == "__main__":
     #创建实例并运行
@@ -226,7 +267,13 @@ if __name__ == "__main__":
 
 
 
-# 全屏模式(待更新)
-# self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-# self.settings.screen_width = self.screen.get_rect().width
-# self.settings.screen_height = self.screen.get_rect().height
+'''
+全屏模式(待更新)
+self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+self.settings.screen_width = self.screen.get_rect().width
+self.settings.screen_height = self.screen.get_rect().height
+
+#随机生成地方对象?
+from random import randint
+random_number = randint(-10, 10)
+ '''
